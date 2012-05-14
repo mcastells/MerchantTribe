f217d0e64c1712ec1135c3af06359a47a4fd050a
Marc Castells <mcastells84@gmail.com>
2012-05-13 22:57:44 -0600

    Product thumbnail styling

--- App/MerchantTribeStore/Images/sites/1/themes/theme-aurora/templates/scripts/ProductThumbnail.js
+++ App/MerchantTribeStore/Images/sites/1/themes/theme-aurora/templates/scripts/ProductThumbnail.js
@@ -1,11 +1,11 @@
 ﻿$(function () {
     $('.record').hover(
         function () {
-            $(this).children('.productInfo').animate({ top: '-=72', opacity: 0.74 });
+            $(this).children('.productInfo').animate({ top: '205', opacity: 0.74 });
             $(this).children('.productImageLink').children('.recordimage').css('background-position', '0px -275px');
         },
         function () {
-            $(this).children('.productInfo').animate({ top: '+=72', opacity: 0.10 });
+            $(this).children('.productInfo').animate({ top: '277', opacity: 0.10 });
             $(this).children('.productImageLink').children('.recordimage').css('background-position', '0px 0px');
         });
 });
