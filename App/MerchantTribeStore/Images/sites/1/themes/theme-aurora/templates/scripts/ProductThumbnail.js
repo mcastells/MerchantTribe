eba6f813854819509506860b22446eff8904955b
Marc Castells <mcastells84@gmail.com>
2012-05-13 18:36:43 -0600

    Product thumbnail styling

---
+++ App/MerchantTribeStore/Images/sites/1/themes/theme-aurora/templates/scripts/ProductThumbnail.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $('.record').hover(
+        function () {
+            $(this).children('.productInfo').animate({ top: '-=72', opacity: 0.74 });
+            $(this).children('.productImageLink').children('.recordimage').css('background-position', '0px -275px');
+        },
+        function () {
+            $(this).children('.productInfo').animate({ top: '+=72', opacity: 0.10 });
+            $(this).children('.productImageLink').children('.recordimage').css('background-position', '0px 0px');
+        });
+});
