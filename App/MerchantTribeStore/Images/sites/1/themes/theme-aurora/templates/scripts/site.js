44984416e7cf36b572e06d01eea4f21ecd0bb44b
bobbychaz <rob@robvolk.com>
2012-06-18 23:20:03 -0600

    select text in qty field on focus

--- App/MerchantTribeStore/Images/sites/1/themes/theme-aurora/templates/scripts/site.js
+++ App/MerchantTribeStore/Images/sites/1/themes/theme-aurora/templates/scripts/site.js
@@ -1,4 +1,7 @@
 ﻿$(function () {
     // bg
     $.backstretch('https://s3.amazonaws.com/illtee-web/layout/bg.jpg');
+
+    // select text in qty field on focus
+    $('input[name="qty"]').focus(function () { $(this).select(); });
 });
