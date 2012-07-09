4fef54ee527540c0549ead08094a34f1b52df2d4
bobbychaz <rob@robvolk.com>
2012-07-09 00:46:32 -0600

    added new aurora.web project for custom functionality

---
+++ App/src/Aurora.Web/Controllers/MailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aurora.Web.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpPost]
+        public ActionResult Signup(string email)
+        {
+            return View();
+        }
+    }
+}
